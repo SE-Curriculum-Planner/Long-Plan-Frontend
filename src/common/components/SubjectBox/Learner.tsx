d785b7e1371635b0053531bed535600b8103b94a
Donteatpineappleonpizza <winittra_saengsroy@cmu.ac.th>
2024-01-30 21:35:40 +0700

    subjectbox update 1

---
+++ src/common/components/SubjectBox/Learner.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const LearnerBox: React.FC = () => {
+  const CourseID = '000000';
+  const CourseTitleEng = 'GE Learner';
+  const TotalCredit = 3;
+  
+
+  return (
+    <div className="inline-flex items-start justify-end gap-[14px] pl-0 pr-[5px] py-0 relative bg-white rounded-[10px] border border-solid border-collection-1-yellow-shade-y5-5 shadow-box-shadow">
+      <div className="relative w-[7px] h-[42px] bg-collection-1-yellow-shade-y5-5 rounded-[10px_0px_0px_10px]" />
+      <div className="relative w-[75px] font-h7 text-black text-[16px] text-center tracking-[0] leading-[21px]">
+        <span className="font-h7 text-black text-[16px] tracking-[0] leading-[21px]">
+          {CourseID}
+          <br />
+        </span>
+        <span className="text-[14px] leading-[19.7px]">{CourseTitleEng}</span>
+      </div>
+      <div className="inline-flex flex-col h-[19px] items-start justify-end gap-[10px] relative flex-[0_0_auto]">
+        <div className="font-h2 text-collection-1-yellow-shade-y5-5 text-[12px] text-center tracking-[0] leading-[15.8px] whitespace-nowrap">
+          {TotalCredit}
+        </div>
+      </div>
+    </div>
+
+
+
+  );
+};
+
+export default LearnerBox;
--- src/modules/home/pages/HomePage.tsx
+++ src/modules/home/pages/HomePage.tsx
@@ -1,5 +1,13 @@
 import { useEffect } from "react";
 import { useLoadingContext } from "react-router-loading";
+import CoreBox from "common/components/SubjectBox/Core";
+import MajorBox from "common/components/SubjectBox/Major";
+import ActBox from "common/components/SubjectBox/Act";
+import LearnerBox from "common/components/SubjectBox/Learner";
+import CoCreBox from "common/components/SubjectBox/Cocre";
+import ElecBox from "common/components/SubjectBox/Elec";
+import FreeBox from "common/components/SubjectBox/Free";
+
 
 function Home() {
 	const loadingContext = useLoadingContext();
@@ -7,7 +15,19 @@ function Home() {
 	useEffect(() => {
 		loadingContext.done();
 	}, []);
-	return <div>Home2</div>;
+	return (
+		
+		<div>
+		  <h2>Welcome to Home</h2>
+		  <CoreBox />
+		  <MajorBox />
+		  <ActBox />
+		  <LearnerBox />
+		  <CoCreBox />
+		  <ElecBox />
+		  <FreeBox />
+		</div>
+	  );
 }
 
 export default Home;
