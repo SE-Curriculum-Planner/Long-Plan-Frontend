add248dbcc9130954c9bec2b660a9fbc770d3bee
Naihtton <whitediary1@outlook.com>
2024-01-31 14:28:39 +0700

    Thian's change

--- src/modules/home/pages/HomePage.tsx
+++ src/modules/home/pages/HomePage.tsx
@@ -1,13 +1,18 @@
+import Flow from "common/components/reactFlow/Flow";
 import { useEffect } from "react";
 import { useLoadingContext } from "react-router-loading";
 
 function Home() {
-	const loadingContext = useLoadingContext();
+  const loadingContext = useLoadingContext();
 
-	useEffect(() => {
-		loadingContext.done();
-	}, []);
-	return <div>Home2</div>;
+  useEffect(() => {
+    loadingContext.done();
+  }, []);
+  return (
+    <div>
+      <Flow />
+    </div>
+  );
 }
 
 export default Home;
