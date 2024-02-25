b6997b08d08a89f4a855c846d25593a251a7c3bc
bobubear <jedsa.pd@gmail.com>
2024-02-26 01:39:42 +0700

    userpage/generaldata

--- src/App.tsx
+++ src/App.tsx
@@ -12,67 +12,76 @@ import { useQuery } from "react-query";
 import { Navigate, useNavigate } from "react-router-dom";
 import { Routes, Route } from "react-router-loading";
 function App() {
-	const navigate = useNavigate();
+    const navigate = useNavigate();
 
-	const { setUserData } = useGlobalStore();
-	// const storeValue = useState({ ...INITIAL_STATE });
-	const { status } = useQuery("init", initData, {
-		staleTime: Infinity,
-		onSuccess: (data) => {
-			if (data) {
-				setUserData(data.userData);
+    const { setUserData } = useGlobalStore();
+    // const storeValue = useState({ ...INITIAL_STATE });
+    const { status } = useQuery("init", initData, {
+        staleTime: Infinity,
+        onSuccess: (data) => {
+            if (data) {
+                setUserData(data.userData);
 
-				if (window.location.pathname !== ClientRouteKey.Home) {
-					navigate(ClientRouteKey.Home, { replace: true });
-				}
-			}
-		},
-	});
+                if (window.location.pathname !== ClientRouteKey.Home) {
+                    navigate(ClientRouteKey.Home, { replace: true });
+                }
+            }
+        },
+    });
 
-	async function initData() {
-		const [data] = await Promise.all([validateLocalToken()]);
+    async function initData() {
+        const [data] = await Promise.all([validateLocalToken()]);
 
-		return data;
-	}
+        return data;
+    }
 
-	function handleError(err: Error) {
-		console.error(err);
-	}
+    function handleError(err: Error) {
+        console.error(err);
+    }
 
-	return (
-		<>
-			<Toaster />
-			<div className=" flex w-full">
-				<FixedLayer>
-					<DebugPanel isDisplayed={!config.isProductionMode} routes={routes} />
-					<AppPageLoader isLoading={status === "loading"} />
-				</FixedLayer>
-				{status === "loading" ? null : status === "success" ? (
-					<Routes>
-						{routes.map(({ path, component: Component, loading = false }) => (
-							<Route
-								key={path}
-								path={path}
-								element={
-									<ErrorBoundary
-										fallback={
-											<div>Something went wrong. Please refresh the page.</div>
-										}
-										onError={handleError}
-									>
-										<Component />
-									</ErrorBoundary>
-								}
-								loading={loading}
-							/>
-						))}
-					</Routes>
-				) : (
-					<Navigate to={ClientRouteKey.Login} replace={true} />
-				)}
-			</div>
-		</>
-	);
+    return (
+        <>
+            <Toaster />
+
+            <FixedLayer>
+                <DebugPanel
+                    isDisplayed={!config.isProductionMode}
+                    routes={routes}
+                />
+                <AppPageLoader isLoading={status === "loading"} />
+            </FixedLayer>
+            {status === "loading" ? null : status === "success" ? (
+                <Routes>
+                    {routes.map(
+                        ({ path, component: Component, loading = false }) => (
+                            <Route
+                                key={path}
+                                path={path}
+                                element={
+                                    <ErrorBoundary
+                                        fallback={
+                                            <div>
+                                                Something went wrong. Please
+                                                refresh the page.
+                                            </div>
+                                        }
+                                        onError={handleError}
+                                    >
+                                        <div className=" flex w-screen h-screen">
+                                            <Component />
+                                        </div>
+                                    </ErrorBoundary>
+                                }
+                                loading={loading}
+                            />
+                        )
+                    )}
+                </Routes>
+            ) : (
+                <Navigate to={ClientRouteKey.Login} replace={true} />
+            )}
+        </>
+    );
 }
 
 export default App;
