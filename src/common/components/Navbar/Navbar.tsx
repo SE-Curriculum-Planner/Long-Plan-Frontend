f19540c6371943a52e0128b640ce3a89a2090292
TonPKG <piyaphat_kh@cmu.ac.th>
2024-03-04 21:36:37 +0700

    nav

--- src/common/components/Navbar/Navbar.tsx
+++ src/common/components/Navbar/Navbar.tsx
@@ -1,21 +1,74 @@
 import "./Navbar.css";
+import { useEffect, useState } from "react";
 interface NavbarProps {
   // Add any props you need, like navigation links
 }
 
 const Navbar: React.FC<NavbarProps> = () => {
+  const [isHome, setIsHome] = useState(false);
+  const [isCreate, setIsCreate] = useState(false);
+  const [isProfile, setIsProfile] = useState(false); // Initialize state for profile image
+
+  useEffect(() => {
+    if (location.pathname === "/user") {
+      setIsProfile(true);
+    } else {
+      setIsProfile(false);
+    }
+    if (location.pathname === "/") {
+      setIsHome(true);
+    } else {
+      setIsHome(false);
+    }
+    if (location.pathname === "/create") {
+      setIsCreate(true);
+    } else {
+      setIsCreate(false);
+    }
+
+  }, [location.pathname]);
   return (
     <nav className="navbar shadow-box-shadow">
       {/* Your navigation content goes here */}
       <ul className="navbar-nav">
         <li className="nav-item">
           <a className="nav-link" href="/">
-            <img src="src/assets/Home_p.png" alt="" style={{ width: "50px" }} />
+            <img src={
+                isHome
+                  ? "src/assets/Home_p.png"
+                  : "src/assets/Home.png"
+              } alt="" style={{ width: "50px" }} />
+          </a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link" href="/create">
+            <img src={
+                isCreate
+                  ? "src/assets/Create_p.png"
+                  : "src/assets/Create.png"
+              } alt="" style={{ width: "50px" }} />
+            Create
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/user">
+          <a className="nav-link" href="/user" >
+            <img
+              src={
+                isProfile
+                  ? "src/assets/Profile_p.png"
+                  : "src/assets/Profile.png"
+              }
+              alt=""
+              style={{ width: "50px" }}
+            />
             Profile
+          </a>
+        </li>
+        <li className="nav-bot">
+          <a className="nav-link" href="/user">
+            <img src="src/assets/Logout.png" alt="" style={{ width: "50px" }} />
+            Log out
           </a>
         </li>
         {/* More links as needed */}
