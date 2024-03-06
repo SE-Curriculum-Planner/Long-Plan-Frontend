3c2b0e9dbbc0ac9365828ba3286e548ff50d1014
Donteatpineappleonpizza <winittra_saengsroy@cmu.ac.th>
2024-03-06 19:19:15 +0700

    edit style

--- src/common/components/Navbar/PlanSelection.tsx
+++ src/common/components/Navbar/PlanSelection.tsx
@@ -1,14 +1,25 @@
 export default function PlanSelection() {
   return (
     <div className="plan-select w-screen">
-      <div className=" mx-auto flex justify-between items-center pb-10 gap-4 mt-10">
+      <div className=" mx-auto flex justify-between items-center pb-10 gap-6 mt-12">
         <h1>หลักสูตรการศึกษา</h1>
         <select
-          className="w-auto h-[40px] rounded-xl"
-          style={{ fontFamily: "IBM Plex Sans Thai, sans-serif" }}
+          className="w-auto h-[40px]  px-4 bg-white rounded-3xl border-2 border-solid border-blue-shadeb5 justify-center items-center gap-2.5 inline-flex"
+          style={{
+            fontFamily: "IBM Plex Sans Thai, sans-serif",
+            fontWeight: "bold",
+            fontSize: "18px",
+            color: "#4351CC",
+          }}
         >
+          {/* <div className="w-80 h-10 px-3 bg-white rounded-3xl border border-solid border-blue-shadeb5 justify-center items-center gap-2.5 inline-flex">
+            <h1 className="text-center text-indigo-700 ">CPE Normal Plan</h1>
+            <div className="px-2 rounded-2xl border border-solid border-zinc-300 justify-center items-center gap-2.5 flex">
+              <span className="text-center text-zinc-300 ">Default</span>
+            </div>
+          </div> */}
+          <option select-text="YES!!!">CPE Normal Plan </option>
           <option select-text="-- EMPTY --">CPE CO-OP Plan </option>
-          <option select-text="YES!!!">CPE Normal Plan</option>
         </select>
         <button className="bg-gray-shadegr6 hover:gray-shadegr6 text-white font-bold py-2 px-4 rounded-xl">
           แก้ไขแพลนนี้
