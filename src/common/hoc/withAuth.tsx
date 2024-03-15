212e9086b600ee0aa53399cc471edfca7c59cf43
JMjirapat <jirapat.prt@gmail.com>
2024-03-16 00:57:27 +0700

    chore: unused import

--- src/common/hoc/withAuth.tsx
+++ src/common/hoc/withAuth.tsx
@@ -7,34 +7,34 @@ import useGlobalStore from "common/contexts/StoreContext";
 import Navbar from "common/components/Navbar/Navbar";
 
 function withAuth(authType: AuthKey) {
-  return <P extends object>(WrappedComponent: React.ComponentType<P>) => {
-    return function WithAuthComponent(props: P) {
-      const loadingContext = useLoadingContext();
-      const { userData } = useGlobalStore();
+	return <P extends object>(WrappedComponent: React.ComponentType<P>) => {
+		return function WithAuthComponent(props: P) {
+			const loadingContext = useLoadingContext();
+			const { userData } = useGlobalStore();
 
-      // for dev
-      if (authType === AuthKey.UserAuth) {
-        return (
-          <>
-            <Navbar />
-            <WrappedComponent {...props} />
-          </>
-        );
-      }
+			// for dev
+			// if (authType === AuthKey.UserAuth) {
+			//   return (
+			//     <>
+			//       <Navbar />
+			//       <WrappedComponent {...props} />
+			//     </>
+			//   );
+			// }
 
-      // for prod
-      // if (authType === AuthKey.UserAuth && userData) {
-      // 	return (
-      // 		<>
-      // 			<Navbar />
-      // 			<WrappedComponent {...props} />
-      // 		</>
-      // 	);
-      // }
-      loadingContext.done();
-      return <Unauth />;
-    };
-  };
+			// for prod
+			if (authType === AuthKey.UserAuth && userData) {
+				return (
+					<>
+						<Navbar />
+						<WrappedComponent {...props} />
+					</>
+				);
+			}
+			loadingContext.done();
+			return <Unauth />;
+		};
+	};
 }
 
 export default withAuth;
--- src/modules/home/pages/HomePage.tsx
+++ src/modules/home/pages/HomePage.tsx
@@ -1,39 +1,36 @@
 import PlanSelection from "common/components/Navbar/PlanSelection";
-import Diagram from "common/components/diagram/flows/ReactFlowDiagram";
 import { useEffect } from "react";
 import { useLoadingContext } from "react-router-loading";
 import EnrollAndCredits from "common/components/enrollAndCredits.tsx";
 import CurriculumBox from "common/components/CurriculumBox";
 
 function Home() {
-  const loadingContext = useLoadingContext();
+	const loadingContext = useLoadingContext();
 
-  useEffect(() => {
-    loadingContext.done();
-  }, []);
-  return (
-    <div
-      className="w-screen flex flex-col"
-      style={{
-        fontFamily: "IBM Plex Sans Thai, sans-serif",
-        backgroundColor: "#F5F5F5",
-      }}
-    >
-      <div className="flex justify-center items-center pt-5">
-        <PlanSelection />
-      </div>
-      <EnrollAndCredits />
-      <h1 className="mt-20 text-center">แผนการเรียน</h1>
-      <CurriculumBox />
-      <div className="w-screen h-screen flex justify-center items-center">
-        <div className="w-3/5 h-4/5 flex  rounded-2xl">
-          {/*<Diagram />*/}
-        </div>
-      </div>
+	useEffect(() => {
+		loadingContext.done();
+	}, []);
+	return (
+		<div
+			className="w-screen flex flex-col"
+			style={{
+				fontFamily: "IBM Plex Sans Thai, sans-serif",
+				backgroundColor: "#F5F5F5",
+			}}
+		>
+			<div className="flex justify-center items-center pt-5">
+				<PlanSelection />
+			</div>
+			<EnrollAndCredits />
+			<h1 className="mt-20 text-center">แผนการเรียน</h1>
+			<CurriculumBox />
+			<div className="w-screen h-screen flex justify-center items-center">
+				<div className="w-3/5 h-4/5 flex  rounded-2xl">{/*<Diagram />*/}</div>
+			</div>
 
-      {/* <CourseTitleExtractor /> */}
-    </div>
-  );
+			{/* <CourseTitleExtractor /> */}
+		</div>
+	);
 }
 
 export default Home;
